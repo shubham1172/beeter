5da11a5cc8e24bb03b0e7970c3dc8a06ce672b07
Shubham Sharma <shubhamsharma1172@gmail.com>
2018-11-11 00:43:14 +0530

    moved likes to front end

--- public/js/home.js
+++ public/js/home.js
@@ -1,11 +1,37 @@
 function textAreaOnFocus(x){
   x.setAttribute("rows", 5)
-  document.getElementById("beet_button").style.display = "";
+  document.getElementById("beet_button").style.display = ""
 }
 
 function textAreaOnBlur(x){
   if (x.value == "") {
     x.setAttribute("rows", 1)
-    document.getElementById("beet_button").style.display = "none";
+    document.getElementById("beet_button").style.display = "none"
   }
+}
+
+function likePressed(e){
+  var beet_id = e.id.split("_")[0]
+  var likeCounter = document.getElementById(`${beet_id}_like_count`) 
+  url = `/beets/like/${beet_id}`
+  console.log("fetching ", url)
+  fetch(url)
+    .then(function(response){
+      if (response.status !== 200){
+        console.log(`Error with ${url}`)
+        return
+      }
+      /** update the counter and logo */
+      var curr = e.children[0].getAttribute('data-prefix')
+      if (curr == "far"){
+        e.children[0].setAttribute('data-prefix', 'fas')
+        likeCounter.innerHTML = parseInt(likeCounter.innerHTML) + 1
+      } else {
+        e.children[0].setAttribute('data-prefix', 'far')
+        likeCounter.innerHTML = parseInt(likeCounter.innerHTML) - 1
+      }
+    })
+    .catch(function(err){
+      console.log(err)
+    })
 }
